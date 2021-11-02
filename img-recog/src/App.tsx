3eea663fa92bc95b0fb651618fc66e7b4c3ba2af
Max Rink <29183162+Mwjrink@users.noreply.github.com>
2021-11-02 19:04:05 -0400

    test

--- img-recog/src/App.tsx
+++ img-recog/src/App.tsx
@@ -1,23 +1,40 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const api_key = '2b10189SmpQJ3XHmESgf2Hz9k'
+
 function App() {
+  const inputFile = useRef<HTMLInputElement | null>(null);
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const onUpload = () => {
+    // `current` points to the mounted file input element
+    if (inputFile.current) {
+      inputFile.current.click();
+    }
+  };
+  
+  const onIdentify = () => {
+    //
+  };
+
+  const handleChange = (event) => {
+    
+    setSelectedImage(value)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={URL.createObjectURL(inputFile.current)} className="App-logo" alt="logo" /> */}
+        <p> {inputFile.current} hello </p>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <input type='file' id='file' ref={inputFile} onChange={handleChange} style={{ display: 'none' }} accept="image/*" />
+        <button onClick={onUpload}>Upload Image</button>
+        <button onClick={onIdentify}>Identify</button>
       </header>
     </div>
   );
