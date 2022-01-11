c56aa317b0d00d452fddfa226f08f1d1c37a6973
Griffin Cook <gm2cook@edu.uwaterloo.ca>
2022-01-10 20:24:22 -0500

    created React app

---
+++ garden-react-app/src/components/MyGardens.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Button from 'react-bootstrap/Button';
+
+function MyGardens() {
+    return(
+        <div>
+            <Button variant="primary">Primary</Button>
+        </div>
+    )
+}
+
+export default MyGardens;
