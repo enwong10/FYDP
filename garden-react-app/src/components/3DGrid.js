40de79e9e33d4f5d0945db49ec8b5e7d0465bd2c
unknown <enwong10@gmail.com>
2022-02-14 18:39:16 -0500

    More mobile styling

--- garden-react-app/src/components/3DGrid.js
+++ garden-react-app/src/components/3DGrid.js
@@ -3,7 +3,6 @@ import Camera, { FACING_MODES } from 'react-html5-camera-photo';
 import { useScreenshot, createFileName } from 'use-react-screenshot'
 import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
-import Button from 'react-bootstrap/Button';
 import garden from '../assets/3d_garden.PNG';
 import 'react-html5-camera-photo/build/css/index.css';
 
@@ -38,49 +37,40 @@ function ARGrid() {
     return (
         <Container ref={ref}>
             {!screenshot ? 
-            <>
+            <div className="position-relative">
                 <Camera onTakePhoto={handleTakePhoto} idealFacingMode={FACING_MODES.ENVIRONMENT} isImageMirror={false}/> 
                 <GardenOverlay src={garden} alt='garden'/>
-                </>
+                </div>
                 : 
-            <div className="d-flex flex-column text-center w-100 h-100">
+            <>
             <Screenshot src={screenshot} alt='screenshot'/>
             <ActionsContainer>
-                <Button onClick={() => setScreenshot(null)}>Take Again</Button>
-                <Button onClick={onDownloadScreenshot}>Save and Exit</Button>
-                <Button onClick={() => navigate('/my-gardens')}>Exit Without Saving</Button>
+                <button class='btn btn-primary' onClick={() => setScreenshot(null)}>Take Again</button>
+                <button class='btn btn-success' onClick={onDownloadScreenshot}>Save and Exit</button>
+                <button class='btn btn-danger' onClick={() => navigate('/my-gardens')}>Exit Without Saving</button>
             </ActionsContainer>
-            </div>
+            </>
             }
         </Container>
     )
 }
 
 const Container = styled.div`
-    position: relative;
+    display: flex;
+    flex-direction: column;
     overflow: hidden;
     width: 100vw;
     height: 100vh;
     height: calc(var(--vh, 1vh) * 100);
-
-    .react-html5-camera-photo {
-        height: 100%;
-        
-        img {
-            height: 100%;
-        }
-
-        video {
-            height: 100%;
-            width: 100%;
-            object-fit: fill;
-        }
-    }
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    background: black;
 `
 
 const Screenshot = styled.img`
     height: 90%;
-    object-fit: fill;
+    object-fit: contain;
 `
 
 const ActionsContainer = styled.div`
@@ -97,11 +87,11 @@ const ActionsContainer = styled.div`
 const GardenOverlay = styled.img`
     position: absolute;
     width: 100%;
-    height: 50%;
+    height: 70%;
     margin: auto;
     left: 0;
     right: 0;
-    bottom: 10%;
+    bottom: 0;
 `
 
 export default ARGrid;
