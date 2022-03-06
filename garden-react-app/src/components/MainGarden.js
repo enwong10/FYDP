466a4fecd5bd326b89a04c0fb98f264c6504ed7c
Jozef Brudnicki <jjbrudni@uwaterloo.ca>
2022-03-06 14:47:42 -0500

    added popups on main page

--- garden-react-app/src/components/MainGarden.js
+++ garden-react-app/src/components/MainGarden.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useRef, useState } from "react";
 import { useNavigate } from 'react-router-dom';
 import styled from "styled-components";
 import add from '../assets/add.svg';
@@ -11,10 +11,78 @@ import settings from '../assets/settings.svg';
 import dictionary from '../assets/dictionary.svg';
 import download from '../assets/download.svg';
 import { Context } from "./Context";
+import Button from 'react-bootstrap/Button';
+import Tooltip from 'react-bootstrap/ToolTip';
+import Popover from 'react-bootstrap/Popover';
+import 'react-html5-camera-photo/build/css/index.css';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
 
 function MainGarden() {
     const navigate = useNavigate();
     const { grid, background } = useContext(Context);
+    const [show, setShow] = useState(false);
+    const target = useRef(null);
+    
+    const renderSettingsPop = (props) => (
+        <Popover id="settings-pop" {...props} 
+                 style={{
+                    backgroundColor: 'green',
+                    color: 'white',
+                    ...props.style,
+                  }}>
+                  Change User Settings
+        </Popover>
+      );
+      const renderDictionaryPop = (props) => (
+        <Popover id="dictionary-pop" {...props} 
+                 style={{
+                    backgroundColor: 'green',
+                    color: 'white',
+                    ...props.style,
+                  }}>
+                  Your Plants Page
+        </Popover>
+      );
+      const renderIDPop = (props) => (
+        <Popover id="id-pop" {...props} 
+                 style={{
+                    backgroundColor: 'green',
+                    color: 'white',
+                    ...props.style,
+                  }}>
+                  Plant Identification Page
+        </Popover>
+      );
+      const renderAddPop = (props) => (
+        <Popover id="add-pop" {...props} 
+                 style={{
+                    backgroundColor: 'green',
+                    color: 'white',
+                    ...props.style,
+                  }}>
+                  Add New Plants To Your Garden
+        </Popover>
+      );
+      const render3DPop = (props) => (
+        <Popover id="3d-pop" {...props} 
+                 style={{
+                    backgroundColor: 'green',
+                    color: 'white',
+                    ...props.style,
+                  }}>
+                  Use our 3D View!
+        </Popover>
+      );
+      const renderDownloadPop = (props) => (
+        <Popover id="down-pop" {...props} 
+                 style={{
+                    backgroundColor: 'green',
+                    color: 'white',
+                    ...props.style,
+                  }}>
+                  Download a JPEG of your Gareden!
+        </Popover>
+      );
 
     const onDownloadClick = () => {
         const a = document.createElement("a");
@@ -25,15 +93,58 @@ function MainGarden() {
 
     return (
         <MainContainer>
-            <Settings role='button' onClick={() => navigate('/settings')}>
-                <img src={settings} alt='settings' />
+            
+            <Settings>
+                <OverlayTrigger 
+                target = {target.current}
+                placement="bottom"
+                delay={{ show: 250, hide: 400 }}
+                overlay={renderSettingsPop}>
+                 <Button ref={target} 
+                style={{backgroundColor: 'transparent', borderColor: "transparent"}}
+                onMouseEnter={() => setShow(!show)}
+                onMouseLeave={() => setShow(!show)}
+                onClick={() => navigate('/settings')}
+                >
+                 <img src={settings} alt='settings' />
+            </Button>
+            </OverlayTrigger>
             </Settings>
-            <Dictionary role='button' onClick={() => navigate('/dictionary')}>
-                <img src={dictionary} alt='dictionary' />
+            <Dictionary> 
+            <OverlayTrigger 
+                target = {target.current}
+                placement="bottom"
+                delay={{ show: 250, hide: 400 }}
+                overlay={renderDictionaryPop}>
+                 <Button ref={target} 
+                style={{backgroundColor: 'transparent', borderColor: "transparent"}}
+                onMouseEnter={() => setShow(!show)}
+                onMouseLeave={() => setShow(!show)}
+                onClick={() => navigate('/dictionary')}
+                >
+                 <img src={dictionary} alt='dictionary' />
+            </Button>
+            </OverlayTrigger>
             </Dictionary>
-            {background && <Download role='button' onClick={onDownloadClick}>
-                    <img src={download} alt='download' />
-                </Download>}
+            {background &&       
+            <Download> 
+                
+                <OverlayTrigger 
+                target = {target.current}
+                placement="bottom"
+                delay={{ show: 250, hide: 400 }}
+                overlay={renderDownloadPop}>
+                 <Button ref={target} 
+                style={{backgroundColor: 'transparent', borderColor: "transparent"}}
+                onMouseEnter={() => setShow(!show)}
+                onMouseLeave={() => setShow(!show)}
+                onClick={() => navigate('/dictionary')}
+                >
+                 <img src={download} alt='download' />
+            </Button>
+            </OverlayTrigger>
+            </Download>}
+
             <MainGardenContainer>
                 {!background ? <>
                     <img src={backgroundPlaceholder} alt='placeholder' />
@@ -42,15 +153,45 @@ function MainGarden() {
             </MainGardenContainer>
             <Footer>
                 <div>
-                    <div role='button' onClick={() => navigate('/2d-grid')}>
+                <OverlayTrigger 
+                target = {target.current}
+                placement="top"
+                delay={{ show: 250, hide: 400 }}
+                overlay={renderAddPop}>
+                    <div role='button' ref={target} 
+                        style={{backgroundColor: 'transparent', borderColor: "transparent"}}
+                        onMouseEnter={() => setShow(!show)}
+                        onMouseLeave={() => setShow(!show)}
+                        onClick={() => navigate('/2d-grid')}>
                         <img src={add} alt='2d-grid' />
                     </div>
-                    <div role='button' onClick={() => navigate('/3d-grid')}>
+                </OverlayTrigger>
+                <OverlayTrigger 
+                target = {target.current}
+                placement="top"
+                delay={{ show: 250, hide: 400 }}
+                overlay={render3DPop}>
+                    <div role='button' ref={target} 
+                        style={{backgroundColor: 'transparent', borderColor: "transparent"}}
+                        onMouseEnter={() => setShow(!show)}
+                        onMouseLeave={() => setShow(!show)}
+                        onClick={() => navigate('/3d-grid')}>
                         <img src={camera} alt='3d-grid' />
                     </div>
-                    <div role='button' onClick={() => navigate('/identification')}>
+                </OverlayTrigger>
+                <OverlayTrigger 
+                target = {target.current}
+                placement="top"
+                delay={{ show: 250, hide: 400 }}
+                overlay={renderIDPop}>
+                    <div role='button' ref={target} 
+                        style={{backgroundColor: 'transparent', borderColor: "transparent"}}
+                        onMouseEnter={() => setShow(!show)}
+                        onMouseLeave={() => setShow(!show)}
+                        onClick={() => navigate('/identification')}>
                         <img src={info} alt='plant-identification' />
                     </div>
+                </OverlayTrigger>    
                 </div>
             </Footer>
         </MainContainer>
@@ -91,7 +232,7 @@ const Settings = styled.div`
 const Dictionary = styled.div`
     position: absolute;
     top: 36px;
-    right: 0;
+    right: 0px;
     margin: 12px;
 `
 
